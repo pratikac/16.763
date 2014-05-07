d8d2b6ba4ba3f947769f69bcd1c56b01f22954ce
pratik.ac <pratik.ac@gmail.com>
2014-05-07 04:36:01 -0400

    (a) done

--- hw4/netrm.py
+++ hw4/netrm.py
@@ -1,40 +1,58 @@
 import numpy as np
 from gurobipy import *
 import pandas as pd
+import pdb
 
-
-airports= {'DEN':0, 'SFO':1, 'MIA':2, 'LAX':3, 'ORD':4}
-classes={'Y':0,'B':1,'M':2}
 cap = [150, 125, 150, 125]
+odif = {}
+demand, fares, legs = {}, {}, {}
 
 def read_data():
-    demand, fares = np.zeros((5,5,3)), np.zeros((5,5,3))
-    leg_map = [[],[],[]]
+    global demand, fares, legs
+    global od, classes
+
     rmdata = np.array(pd.read_csv('denhub.csv'))
+
     for r in rmdata:
-        r[1], r[2] = airports[r[1]], airports[r[2]]
-        r[3] = classes[r[3]]
-        fares[r[1],r[2],r[3]] = r[-1]
-        demand[r[1],r[2],r[3]] = r[-2]
+        t1 = (r[1],r[2],r[3])
+        if not t1 in odif.keys():
+            odif[t1] = 1
 
-        legs = r[0].split(' ')
-        for l in legs:
-            leg_map[int(l)-1].append([r[1],r[2],r[3]])
+        demand[t1] = r[4]
+        fares[t1] = r[5]
 
-    return demand, fares, leg_map
+        t3 = r[0].split(' ')
+        t3 = [int(t3i) for t3i in t3]
+        for t3i in t3:
+            if not t3i in legs:
+                legs[t3i] = []
+            legs[t3i].append(t1)
 
+    return
 
 def solve_netrm():
-    demand, fares, leg_map = read_data()
     m = Model('netrm')
-    x = m.addVar(vtype=GRB.INTEGER, name='x')
+    x = {}
+    for i,j,k in odif.keys():
+        x[i,j,k] = m.addVar(vtype='I', ub=demand[i,j,k], obj=-fares[i,j,k],
+                            name='x[%s,%s,%s]' %(i,j,k))
     m.update()
 
-    x = mi.IntVar((5,5,3))
-    f = 0.x[0,0,0]
-    f = sum([sum([ sum([x[i,j,k] for k in xrange(3)]) for j in xrange(5)]) \
-            for i in xrange(5)])
-    import pdb; pdb.set_trace()
+    # leg capacity
+    for l in legs.keys():
+        m.addConstr(
+            quicksum(x[xi] for xi in legs[l]) <= cap[l-1],
+                    'leg[%s]'%(l))
 
+    m.optimize()
 
+    # print sol
+    #pdb.set_trace()
+    if m.status == GRB.status.OPTIMAL:
+        #pdb.set_trace()
+        #sol = m.getAttr('x',x)
+        for i,j,k in odif.keys():
+            print('%s,%s,%s -> %s, ub: %s') %(i,j,k,x[i,j,k].x,demand[i,j,k])
+
+read_data()
 solve_netrm()
